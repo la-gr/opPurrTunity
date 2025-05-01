a817b47fcd605abb76e76ea46ade4893d05897e0
Emily Wu <emily.wu0718@gmail.com>
2025-05-01 13:28:10 -0400

    chat into cnavas

--- public/main.js
+++ public/main.js
@@ -33,7 +33,7 @@ offscreenCanvas.height = gridHeight;
 
 let selectedColour = "#000000"
 let selectedTool = "brush";
-let draw = false;
+let draw = true;
 
 let cursorX = 0;
 let cursorY = 0;
@@ -45,6 +45,9 @@ ctx.strokeRect(cursorX * pixelSize, cursorY * pixelSize, pixelSize, pixelSize);
 
 //paint if buttons are pressed
 document.addEventListener("keydown", (e) => {
+    if (draw === false){
+        return;
+    }
     let moved = false;
 
     if (e.key === "w") {
@@ -93,10 +96,90 @@ const clear = document.getElementById("clear");
 clear.addEventListener("click", (e) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     offCtx.clearRect(0, 0, canvas.width, canvas.height);
-    window.location.href = "chat.html";
 
     //keep cursor highlighted
     ctx.strokeStyle = "#008080FF";
     ctx.lineWidth = 2;
     ctx.strokeRect(cursorX * pixelSize, cursorY * pixelSize, pixelSize, pixelSize);
 });
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "e") {
+        if(draw === true){
+            draw = false;
+            console.log(draw);
+        } else if (draw===false){
+            draw = true;
+        }
+    }
+})
+
+//CHAT
+document.getElementById("chat");
+
+if (draw === false)  {
+    const socket = io();
+    console.log("in");
+    let colour = generateRandomColor(); //sets the colour of the text
+    function generateRandomColor() {
+        let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        if (randomColor.length !== 7) { //if the color code is somehow invalid
+            randomColor = generateRandomColor();
+        }
+        return randomColor;
+    }
+
+    const messages = document.getElementById('messages');
+    const cats = ["meow", "hiss", "purr", "miaou"]; //cat sounds
+    const dogs = ["bark", "woof", "arf", "ruff"]; //dog sounds
+    let language = cats; //current language that the buttons will enter
+    let n = 0;
+
+    //message if buttons are pressed
+    let msg = "";
+    document.addEventListener("keydown", (e) => {
+        if(draw===true){
+            return;
+        }
+        e.preventDefault();
+        //enters a cat or dog sound on button click
+        if (e.key === "w") {
+            console.log("here")
+            msg = language[3];
+            socket.emit('chat message', {msg, colour});
+        }
+        if (e.key === "s") {
+            msg = language[0];
+            socket.emit('chat message', {msg, colour});
+        }
+        if (e.key === "a") {
+            msg = language[1];
+            socket.emit('chat message', {msg, colour});
+        }
+        if (e.key === "d") {
+            msg = language[2];
+            socket.emit('chat message', {msg, colour});
+        }
+        if (e.key === "e") { //goes back to canvas
+            window.location.href = "index.html";
+        }
+        if (e.key === "f") { //changes to dog or cat language
+            if (n === 1){
+                language = cats;
+                n = 0;
+            } else{
+                language = dogs;
+                n = 1;
+            }
+        }
+    });
+
+    socket.on('chat message', ({msg, colour}) => {
+        const item = document.createElement('li');
+        item.style.color = colour;
+        item.textContent = msg; //set the item to the user's entered message
+        messages.appendChild(item); //add the message to the ul
+        window.scrollTo(0, document.chat.scrollHeight); //scrolls the messages
+        //socket.auth.serverOffset = serverOffset;
+    });
+}
